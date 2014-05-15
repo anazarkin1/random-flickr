87fd8eafce295eb922d6b9cc28643fe12cbd27cd
alex nazarkin <anazarkin1@gmail.com>
2014-05-15 15:07:42 -0400

    Added loader gif;

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -4,12 +4,15 @@
 
 function MainCtrl($scope, $http, $routeParams){
     $scope.search = function(){
+        //hides img with potential pic, shows a loader gif
         $scope.form.hideImage=true;
+
+        //call api to get a new pic
         $http.get('/api/randomPick/' + $scope.form.keyword).
             success(function(data){
-
-                console.log(data);
                 data.keyword = $scope.form.keyword;
+                //Check status of json
+
                 if (data.stat==="ok"){
                     $scope.form =data;
                     $scope.form.hideImage= false;
