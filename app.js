6a349ccc9170bbc61f1dacdbeec69472724a8e98
alex nazarkin <anazarkin1@gmail.com>
2014-05-20 15:54:39 -0400

    heroku fix

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ var app = module.exports = express();
  */
 
 // all environments
-app.set('port', process.env.PORT || 5000);
+app.set('port', 5000);
 app.set('views', __dirname + '/views');
 // app.set('view engine', 'jade');
 app.engine('html', require('ejs').renderFile);
